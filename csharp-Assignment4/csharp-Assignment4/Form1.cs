1d3038c6aff42ad2cc29e44b441409c053e7230a
Goldenlady0940 <sebrinaa18@gmail.com>
2022-07-09 22:26:03 +0300

    Student Management

---
+++ csharp-Assignment4/csharp-Assignment4/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_Assignment4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Register_Click(object sender, EventArgs e)
+        {
+            Student s = new Student
+            {
+                Name = txt_Name.Text,
+                Id = int.Parse(txt_Id.Text),
+                Gender = txt_Gender.Text,
+                Field = txt_Field.Text,
+                Date_Of_Registeration = txt_DatePicker.Value
+            };
+            s.Register();
+            
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            Student s = new Student();
+                s.Cancel();
+        }
+    }
+}
---
+++ csharp-Assignment4/csharp-Assignment4/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_Assignment4
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Gender { get; set; }
+        public DateTime Date_Of_Registeration { get; set; }
+        public string Field { get; set; }
+
+        public void Register()
+        {
+            MessageBox.Show("Registered Successfully!");
+        }
+
+        public void Cancel()
+        {
+            MessageBox.Show("Exited");
+        }
+    }
+}
