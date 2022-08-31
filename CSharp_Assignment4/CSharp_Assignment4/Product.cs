9a8e27af41257b5788479913b2dab1c93a07ff0d
Goldenlady0940 <sebrinaa18@gmail.com>
2022-08-31 23:05:08 +0300

    passing as a parameter
    to prevent sql injection attack

--- CSharp_Assignment4/CSharp_Assignment4/Product.cs
+++ CSharp_Assignment4/CSharp_Assignment4/Product.cs
@@ -33,30 +33,35 @@ namespace CSharp_Assignment4
         {
             return Products.Find(p => p.Object_Name == name); // arrow function
         }
-        //new get manager
-        //class product extend entity
+       
         public void save()
         {
             SqlConnection con = new SqlConnection(connection);
             con.Open();
-            string Query = "INSERT INTO PRODUCT VALUES (" + this.Number + ",'" + this.Date + "'," + this.Inventory_Number + ",'" + this.Object_Name + "'," + this.Count + "," + this.Price + ",'" + this.isAvailable + "','" + this.rad_Plastic + "');";//this.Productname
-            SqlCommand cmd = new SqlCommand(Query, con);
-            /*
-             * sql injection attack user yerasu query indayasgeba
-             * string Query = "INSERT INTO PRODUCT VALUES (@name,@int)
-             * SqlCommand cmd = new SqlCommand(Query, con);
-             * cmd.parameters.AddValluewith("@name",this.name)//@--is the parameter and this.name is to replace the 
-             * */
+             string Query = "INSERT INTO PRODUCT VALUES (@ID,@DATE,@INV_NUM,@NAME,@COUNT,@PRICE,@AVAIL,@PLASTIC);";//this.Productname
+             SqlCommand cmd = new SqlCommand(Query, con);
+            cmd.Parameters.AddWithValue("@ID", this.Number);
+            cmd.Parameters.AddWithValue("@DATE", this.Date);
+            cmd.Parameters.AddWithValue("@INV_NUM",this.Inventory_Number);
+            cmd.Parameters.AddWithValue("@NAME", this.Object_Name);
+            cmd.Parameters.AddWithValue("@COUNT", this.Count);
+            cmd.Parameters.AddWithValue("@PRICE", this.Price);
+            //cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("@AVAIL", System.Data.SqlDbType.VarChar).Value = this.isAvailable;//value
+            cmd.Parameters.Add("@PLASTIC", System.Data.SqlDbType.VarChar).Value = this.rad_Plastic;
+            //cmd.ExecuteScalar();
+            //spGetAllProduct
+        
             var result = cmd.ExecuteNonQuery();
             con.Close();
             //Products.Add(this);
             //MessageBox.Show("connected");
-
+            
         }
         public static List<Product> GetAllProduct()
         {
             string connection = @"Data Source=DESKTOP-O813SED\SQLEXPRESS;Initial Catalog=CCONNECTION;Integrated Security=True";
-
+            
             SqlConnection con = new SqlConnection(connection);
             con.Open();
             string Query = "SELECT * FROM PRODUCT";//this.Productname
@@ -71,15 +76,15 @@ namespace CSharp_Assignment4
                 p.Inventory_Number = (int)result["INV_NUM"];
                 p.Object_Name = (string)result["NAME"];
                 p.Count = (int)result["COUN"];
-                p.Price = (int)result["PRICE"];
+                p.Price =(int) result["PRICE"];
                 p.isAvailable = Convert.ToBoolean(result["AVAILABILITY"]);
                 p.rad_Plastic = Convert.ToBoolean(result["PLASTIC"]);
                 p.rad_NonPlastic = !(Convert.ToBoolean(result["PLASTIC"]));
                 temp.Add(p);
             }
             con.Close();
-            return temp;
-            // return Products;
+                return temp;
+               // return Products;
         }
 
     }
